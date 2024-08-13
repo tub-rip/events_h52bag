3dfb68a0a83af39e80283d7f264c8ea377934733
ghoshsuman <sumanghosh29@gmail.com>
2024-08-13 17:51:47 +0200

    produce only a single bag as output. Improved memory allocation.

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ using std::endl;
 using namespace H5;
 
 
-void readH5Datasets(std::string fname, std::string dataset, std::vector<double> &data, hsize_t hslab_offset, hsize_t hslab_count){
+void readH5Datasets(std::string fname, std::string dataset, std::vector<uint64_t> &data, hsize_t hslab_offset, hsize_t hslab_count){
 
     H5File file( fname.c_str(), H5F_ACC_RDONLY );
     cout<<"Reading dataset "<<dataset<<endl;
@@ -38,7 +38,7 @@ void readH5Datasets(std::string fname, std::string dataset, std::vector<double> 
         // create a vector the same size as the dataset
         data.resize(1);
         cout<<"Vectsize: "<<data.size()<<endl;
-        dset.read(data.data(), PredType::NATIVE_DOUBLE, H5S_ALL, H5S_ALL);
+        dset.read(data.data(), PredType::NATIVE_UINT64, H5S_ALL, H5S_ALL);
     }
     else { // for array datasets
         // Get the dimension size of each dimension in the dataspace and display them.
@@ -52,18 +52,18 @@ void readH5Datasets(std::string fname, std::string dataset, std::vector<double> 
         hsize_t count[1];
         offset[0] = hslab_offset;
         count[0] = std::min(hslab_count, dims_out[0] - hslab_offset);
-        cout<<"count: "<< count[0]<<" "<<offset[0]<<endl;
+        cout<<"count: "<< count[0]<<" offset: "<<offset[0]<<endl;
         dataspace.selectHyperslab(H5S_SELECT_SET, count, offset);
 
         // Define the memory dataspace
         DataSpace memspace (1, count);
 
         // create a vector the same size as the dataset
-        data.resize(count[0]);
+        data.resize(count[0], 0);
         cout<<"Vectsize: "<<data.size()<<endl;
 
         // pass pointer to the array (or vector) to read function, along with the data type and space.
-        dset.read(data.data(), PredType::NATIVE_DOUBLE, memspace, dataspace);
+        dset.read(data.data(), PredType::NATIVE_UINT64, memspace, dataspace);
 
     }
 
@@ -72,8 +72,8 @@ void readH5Datasets(std::string fname, std::string dataset, std::vector<double> 
 }
 
 int main(int argc, char *argv[]){
-    int numev_per_bag = 500000000; //500M
-    int packet_size = 100000;    
+    int numev_per_iter = 500000000; //500M
+    int packet_size = 100000;
     std::string dataset_prefix = "events";
     if (argc<6){
         cout<<"Too few arguments.. Exiting!\n";
@@ -86,7 +86,7 @@ int main(int argc, char *argv[]){
     int width = std::stoi(argv[5]);
 
     if (argc == 7){
-        numev_per_bag = std::stoi(argv[6]);
+        numev_per_iter = std::stoi(argv[6]);
     }
     if (argc == 8){
         packet_size = std::stoi(argv[7]);
@@ -103,23 +103,21 @@ int main(int argc, char *argv[]){
     {
         hsize_t hslab_offset = 0;
         int i=0;
+        rosbag::Bag bag;
+        bag.open(bagname+".bag", rosbag::bagmode::Write);
         while(true){
-            std::vector<double> data;
-            readH5Datasets(fname, dataset_prefix + "/t", data, hslab_offset, numev_per_bag);
-            std::vector<double> t(data);
-            data.resize(0);
-            readH5Datasets(fname, dataset_prefix + "/x", data, hslab_offset, numev_per_bag);
+            std::vector<uint64_t> data;
+            readH5Datasets(fname, dataset_prefix + "/t", data, hslab_offset, numev_per_iter);
+            std::vector<uint64_t> t(data.begin(), data.end());
+            readH5Datasets(fname, dataset_prefix + "/x", data, hslab_offset, numev_per_iter);
             std::vector<uint16_t> x(data.begin(), data.end());
-            data.resize(0);
-            readH5Datasets(fname, dataset_prefix + "/y", data, hslab_offset, numev_per_bag);
+            readH5Datasets(fname, dataset_prefix + "/y", data, hslab_offset, numev_per_iter);
             std::vector<uint16_t> y(data.begin(), data.end());
-            data.resize(0);
-            readH5Datasets(fname, dataset_prefix + "/p", data, hslab_offset, numev_per_bag);
+            readH5Datasets(fname, dataset_prefix + "/p", data, hslab_offset, numev_per_iter);
             std::vector<uint8_t> p(data.begin(), data.end());
-            data.resize(0);
-            readH5Datasets(fname, "t_offset", data, hslab_offset, numev_per_bag);
-            std::vector<double> offset_data(data.begin(), data.end());
-            data.resize(0);
+            readH5Datasets(fname, "t_offset", data, hslab_offset, numev_per_iter);
+            std::vector<uint64_t> offset_data(data.begin(), data.end());
+            std::vector<uint64_t>().swap(data); //clearing memory
 
             float t_offset;
             if (offset_data.size()>0)
@@ -127,37 +125,36 @@ int main(int argc, char *argv[]){
             else
                 t_offset = 0;
 
-            cout<<"Writing to rosbag #"<<i<<" at offset "<<hslab_offset<<" containing "<<t.size()<<" events"<<endl;
+            cout<<"Writing to rosbag #iteration"<<i<<" at offset "<<hslab_offset<<" containing "<<t.size()<<" events"<<endl;
             // Write to rosbag
-            rosbag::Bag bag;
-            bag.open(bagname+"_"+std::to_string(i)+".bag", rosbag::bagmode::Write);
+
             dvs_msgs::EventArray evQueue;
-            for (int i=0; i<t.size(); i++){
+            for (int j=0; j<t.size(); j++){
                 dvs_msgs::Event ev;
-                ev.ts.fromSec((t[i]+t_offset)/1e6);
-                ev.x = x[i];
-                ev.y = y[i];
-                ev.polarity = (int)p[i];
+                ev.ts.fromSec((t[j]+t_offset)/1e6);
+                ev.x = x[j];
+                ev.y = y[j];
+                ev.polarity = (int)p[j];
                 evQueue.events.push_back(ev);
-                if(i % packet_size == 0){
-//                    cout<<"writing msg "<<i*100./t.size()<<"%"<<endl;
+                if(j % packet_size == 0){
+                    //                    cout<<"writing msg "<<i*100./t.size()<<"%"<<endl;
                     evQueue.header.stamp = ev.ts;
-                    evQueue.header.frame_id = i;
+                    evQueue.header.frame_id = j;
                     evQueue.height = height;
                     evQueue.width = width;
                     bag.write(topic, ev.ts, evQueue);
                     evQueue.events.clear();
                 }
             }
-            bag.close();
-            if(t.size()!=numev_per_bag){
+            if(t.size()!=numev_per_iter){
                 cout<<"Final bag file has been written."<<endl;
                 break;
             }
-            hslab_offset+=numev_per_bag;
+            hslab_offset+=numev_per_iter;
             i++;
         }
 
+        bag.close();
 
     }  // end of try block
     // catch failure caused by the H5File operations
